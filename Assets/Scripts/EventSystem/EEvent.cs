6cea5dae52fbfeccbd7a21adc3106d269ce5801e
wuduanyi <wdy219813@126.com>
2020-04-27 22:42:20 +0800

    初始项目提交

---
+++ Assets/Scripts/EventSystem/EEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jundian.EventSystem
+{
+    public enum EEvent
+    {
+        // 添加你自己需要的事件类型
+        TestEvent = 0,
+    }
+}
---
+++ Assets/Scripts/EventSystem/EventTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Jundian.EventSystem;
+
+public class EventTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Register();
+        PostEvent();
+    }
+
+    void Register()
+    {
+        EventSystem.Instance.RegisterEvent<string, int>(EEvent.TestEvent, OnRecvEvent);
+    }
+
+
+    void PostEvent()
+    {
+        EventSystem.Instance.PostEvent(EEvent.TestEvent, "TestChar", 2);
+    }
+
+    void OnRecvEvent(string a, int b)
+    {
+        Debug.Log(a + b);
+    }
+}
